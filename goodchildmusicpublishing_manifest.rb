2796658247ceb27d921baa08bc4cee1b18c124d2
tharealpatton <pj@crushlovely.com>
2009-03-12 19:52:13 -0700

    Added manifests for both apps.

---
+++ goodchildmusicpublishing_manifest.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/lib/rails_manifest.rb'
+
+# This class is for a *single* application. This could be easily implemented to load json/yaml.
+class Goodchildmusicpublishing < RailsManifest
+
+  # name your app
+  name "goodchildmusicpublishing"
+  domain "goodchildmusicpublishing.com"
+  database_password "secret"
+
+  # add your gems and other good stuff here
+  def application_packages
+    # package "some_awesome_gem", :ensure => :installed, :provider => :gem
+    # package "some_awesome_native_package", :ensure => :installed
+  end
+end
